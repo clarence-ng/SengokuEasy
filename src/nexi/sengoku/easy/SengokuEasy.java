bac031127f115ea2237926e35159e56e64b2d72c
Lawrence <Lawrence@192.168.1.104>
2010-11-21 01:26:19 -0800

    Attempt to fix fucked up encoding
    ゲームスタート

--- src/nexi/sengoku/easy/SengokuEasy.java
+++ src/nexi/sengoku/easy/SengokuEasy.java
@@ -23,7 +23,7 @@ public class SengokuEasy {
 		logger.info("logged in to yahoo");
 
 		logger.info("logging in to world server");
-		HtmlPage worldsPage = page.getAnchorByText("���`�ॹ���`��").click();
+		HtmlPage worldsPage = page.getAnchorByText("ゲームスタート").click();
 		logger.info("logged in to world server");
 		
 		World world9 = new World(9, worldsPage.getUrl().toString());
